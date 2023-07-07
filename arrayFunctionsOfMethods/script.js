302adccd3a8308d41b2ad57381c2e073851624f3
twlalaa <bloglala2003@gmail.com>
2023-07-07 20:01:08 +0400

    Add new tasks

--- arrayFunctionsOfMethods/script.js
+++ arrayFunctionsOfMethods/script.js
@@ -9,59 +9,53 @@ let moviesArray = [
   "The Truman Show",
 ];
 
-console.log(moviesArray);
+// console.log(moviesArray);
 
 //pop
 
-// moviesArray.pop();
-
-// console.log(moviesArray);
+// console.log(moviesArray.pop());
 
 //pop with function
 
 const popFunction = (array) => {
-  array.length = array.length - 1;
-  return array;
+  let removedPop = array[array.length - 1];
+  array.length -= 1;
+  return removedPop;
 };
 
 // console.log(popFunction(moviesArray));
 
 //push
 
-// moviesArray.push("Black Swan");
-
-// console.log(moviesArray);
+// console.log(moviesArray.push("Black Swan"));
 
 //push with function
 
 const pushFunction = (array, value) => {
   array[array.length] = value;
-  return array;
+  return array.length;
 };
 
 // console.log(pushFunction(moviesArray, "Black Swan"));
 
 //shift
 
-// moviesArray.shift();
-
-// console.log(moviesArray);
+// console.log(moviesArray.shift());
 
 let shiftFunction = (array) => {
+  let removed = array[0];
   for (let i = 0; i < array.length; i++) {
     array[i] = array[i + 1];
   }
   array.length -= 1;
-  return array;
+  return removed;
 };
 
 // console.log(shiftFunction(moviesArray));
 
 //unshift
 
-// moviesArray.unshift("Black Swan");
-
-// console.log(moviesArray);
+// console.log(moviesArray.unshift("Black Swan"));
 
 //unshift with function
 
@@ -71,7 +65,91 @@ const unshiftFunction = (array, value) => {
     array[i] = array[i - 1];
   }
   array[0] = value;
-  return array;
+  return array.length;
 };
 
 // console.log(unshiftFunction(moviesArray, "Black Swan"));
+
+//reverse
+
+// console.log(moviesArray.reverse());
+
+const reverseFunction = (array) => {
+  let newarray = [];
+  for (let i = moviesArray.length - 1; i >= 0; i--) {
+    for (let j = 0; j < moviesArray.length; j++) {
+      newarray[j] = array[i];
+    }
+  }
+  return newarray;
+};
+
+// reverseFunction(moviesArray);
+
+const mynewreverse = (array) => {
+  let newarray = [];
+  let i = 0;
+  let j = array.length - 1;
+  while (i < array.length) {
+    newarray[j] = array[i];
+    i++;
+    j--;
+  }
+  return newarray;
+};
+
+// console.log(mynewreverse(moviesArray));
+
+const lastReverse = (array) => {
+  let newarray = [];
+  let j = 0;
+  for (let i = moviesArray.length - 1; i >= 0; i--) {
+    newarray[j] = array[i];
+    j++;
+  }
+  return newarray;
+};
+
+// console.log(lastReverse(moviesArray));
+
+//vowel function
+
+const input = prompt("Please enter your name: ");
+
+const vowelFunction = (string) => {
+  let vowels = [
+    "a",
+    "ı",
+    "o",
+    "u",
+    "e",
+    "ə",
+    "i",
+    "ö",
+    "ü",
+    "A",
+    "I",
+    "O",
+    "U",
+    "E",
+    "Ə",
+    "İ",
+    "Ö",
+    "Ü",
+  ];
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (string[i] == vowels[j]) {
+        count++;
+      }
+    }
+  }
+  console.log(
+    count == 1
+      ? `Your name has only one vowel!`
+      : `Your name has ${count} vowels!`
+  );
+};
+
+// vowelFunction(input);
